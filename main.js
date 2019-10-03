d3e5cc54c7ef22297faaf91b2d52a548bede47f9
kuikar <karin.kuipers@kartverket.no>
2019-10-03 10:33:19 +0200

    inistial commit of main files

---
+++ main.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFha2EiLCJhIjoiY2lzZnp4eWhvMDAzejJwbnExNmY5ODdsMCJ9.EeEx5fDH-zH3pdvuHMuncg';
+var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'style.json',
+        hash: true,
+        zoom: 13.479533004671785,
+        pitch: 0,
+        bearing: 0,
+        center: [ 5.326969379041202, 60.39360277336942]
+    });
